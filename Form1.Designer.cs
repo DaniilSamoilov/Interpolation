c7200623ca700831310de36feb547fff4a159e2e
DaniilSamoilov <danya.sam2@yandex.ru>
2024-06-02 00:59:43 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Interpolation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.ArgumentTable = new System.Windows.Forms.DataGridView();
+            this.GenTableBtn = new System.Windows.Forms.Button();
+            this.PointCount = new System.Windows.Forms.TextBox();
+            this.LagranjBtn = new System.Windows.Forms.Button();
+            this.CubicSplineBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ArgumentTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Location = new System.Drawing.Point(13, 13);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(775, 316);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // ArgumentTable
+            // 
+            this.ArgumentTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ArgumentTable.Location = new System.Drawing.Point(13, 335);
+            this.ArgumentTable.Name = "ArgumentTable";
+            this.ArgumentTable.Size = new System.Drawing.Size(775, 68);
+            this.ArgumentTable.TabIndex = 1;
+            // 
+            // GenTableBtn
+            // 
+            this.GenTableBtn.Location = new System.Drawing.Point(119, 409);
+            this.GenTableBtn.Name = "GenTableBtn";
+            this.GenTableBtn.Size = new System.Drawing.Size(109, 40);
+            this.GenTableBtn.TabIndex = 2;
+            this.GenTableBtn.Text = "Установить количество точек";
+            this.GenTableBtn.UseVisualStyleBackColor = true;
+            this.GenTableBtn.Click += new System.EventHandler(this.GenTableBtn_Click);
+            // 
+            // PointCount
+            // 
+            this.PointCount.Location = new System.Drawing.Point(12, 420);
+            this.PointCount.Name = "PointCount";
+            this.PointCount.Size = new System.Drawing.Size(100, 20);
+            this.PointCount.TabIndex = 3;
+            // 
+            // LagranjBtn
+            // 
+            this.LagranjBtn.Location = new System.Drawing.Point(234, 409);
+            this.LagranjBtn.Name = "LagranjBtn";
+            this.LagranjBtn.Size = new System.Drawing.Size(119, 40);
+            this.LagranjBtn.TabIndex = 4;
+            this.LagranjBtn.Text = "Построить полином Лагранжа";
+            this.LagranjBtn.UseVisualStyleBackColor = true;
+            this.LagranjBtn.Click += new System.EventHandler(this.LagranjBtn_Click);
+            // 
+            // CubicSplineBtn
+            // 
+            this.CubicSplineBtn.Location = new System.Drawing.Point(360, 409);
+            this.CubicSplineBtn.Name = "CubicSplineBtn";
+            this.CubicSplineBtn.Size = new System.Drawing.Size(120, 40);
+            this.CubicSplineBtn.TabIndex = 5;
+            this.CubicSplineBtn.Text = "Построить кубический сплайн";
+            this.CubicSplineBtn.UseVisualStyleBackColor = true;
+            this.CubicSplineBtn.Click += new System.EventHandler(this.CubicSplineBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CubicSplineBtn);
+            this.Controls.Add(this.LagranjBtn);
+            this.Controls.Add(this.PointCount);
+            this.Controls.Add(this.GenTableBtn);
+            this.Controls.Add(this.ArgumentTable);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ArgumentTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridView ArgumentTable;
+        private System.Windows.Forms.Button GenTableBtn;
+        private System.Windows.Forms.TextBox PointCount;
+        private System.Windows.Forms.Button LagranjBtn;
+        private System.Windows.Forms.Button CubicSplineBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Interpolation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ArgumentTable.RowCount = 2;
+            ArgumentTable.Rows[0].HeaderCell.Value = "x";
+            ArgumentTable.Rows[1].HeaderCell.Value = "y";
+        }
+
+        private void GenTableBtn_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(PointCount.Text, out int numPoints) && numPoints > 0)
+            {
+                ArgumentTable.ColumnCount = 1;
+                ArgumentTable.Columns[0].HeaderCell.Value = "x1";
+                for (int i = 1; i < numPoints; i++)
+                {
+                    ArgumentTable.Columns.Add("","x"+(i+1));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number of points.");
+            }
+        }
+        private void PlotLagrangePolynomial()
+        {
+            // Получение данных из DataGridView
+            List<double> xValues = new List<double>();
+            List<double> yValues = new List<double>();
+
+            DataGridViewRow xRow = ArgumentTable.Rows[0];
+            DataGridViewRow yRow = ArgumentTable.Rows[1];
+
+            for (int i = 0; i < ArgumentTable.Columns.Count; i++) // Начинаем с 1, чтобы пропустить столбцы "X" и "Y"
+            {
+                if (xRow.Cells[i].Value != null && yRow.Cells[i].Value != null)
+                {
+                    try
+                    {
+                        double x = Convert.ToDouble(xRow.Cells[i].Value);
+                        double y = Convert.ToDouble(yRow.Cells[i].Value);
+                        xValues.Add(x);
+                        yValues.Add(y);
+                    }
+                    catch (FormatException)
+                    {
+                        MessageBox.Show("Please enter valid numeric values.");
+                        return;
+                    }
+                }
+            }
+
+
+            // Очистка предыдущих серий
+            chart1.Series.Clear();
+
+            // Добавление точек на график
+            var seriesPoints = new Series("Points")
+            {
+                ChartType = SeriesChartType.Point,
+                Color = Color.Red
+            };
+            
+
+            for (int i = 0; i < xValues.Count; i++)
+            {
+                seriesPoints.Points.AddXY(xValues[i], yValues[i]);
+            }
+
+
+            // Построение полинома Лагранжа
+            var seriesLagrange = new Series("Lagrange Polynomial")
+            {
+                ChartType = SeriesChartType.Point,
+                Color = Color.Blue
+            };
+
+            for (double x = xValues[0]; x <= xValues[xValues.Count - 1]; x += 0.01)
+            {
+                double y = LagrangePolynomial(x, xValues.ToArray(), yValues.ToArray());
+                seriesLagrange.Points.AddXY(x, y);
+            }
+
+            chart1.Series.Add(seriesLagrange);
+            chart1.Series.Add(seriesPoints);
+        }
+        private double LagrangePolynomial(double x, double[] xValues, double[] yValues)
+        {
+            double result = 0.0;
+            int n = xValues.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                double term = yValues[i];
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != j)
+                    {
+                        term *= (x - xValues[j]) / (xValues[i] - xValues[j]);
+                    }
+                }
+                result += term;
+            }
+
+            return result;
+        }
+        private void LagranjBtn_Click(object sender, EventArgs e)
+        {
+            PlotLagrangePolynomial();
+        }
+
+        private void CubicSplineBtn_Click(object sender, EventArgs e)
+        {
+            // Получение данных из DataGridView
+            List<double> xValues = new List<double>();
+            List<double> yValues = new List<double>();
+
+            DataGridViewRow xRow = ArgumentTable.Rows[0];
+            DataGridViewRow yRow = ArgumentTable.Rows[1];
+
+            for (int i = 0; i < ArgumentTable.Columns.Count; i++) // Начинаем с 1, чтобы пропустить столбцы "X" и "Y"
+            {
+                if (xRow.Cells[i].Value != null && yRow.Cells[i].Value != null)
+                {
+                    try
+                    {
+                        double x = Convert.ToDouble(xRow.Cells[i].Value);
+                        double y = Convert.ToDouble(yRow.Cells[i].Value);
+                        xValues.Add(x);
+                        yValues.Add(y);
+                    }
+                    catch (FormatException)
+                    {
+                        MessageBox.Show("Please enter valid numeric values.");
+                        return;
+                    }
+                }
+            }
+
+
+            // Очистка предыдущих серий
+            chart1.Series.Clear();
+
+            // Добавление точек на график
+            var seriesPoints = new Series("Points")
+            {
+                ChartType = SeriesChartType.Point,
+                Color = Color.Red
+            };
+
+
+            for (int i = 0; i < xValues.Count; i++)
+            {
+                seriesPoints.Points.AddXY(xValues[i], yValues[i]);
+            }
+            if (xValues.Count > 1)
+            {
+                // Интерполяция кубическим сплайном
+                (double[] xs, double[] ys) = InterpolateSpline(xValues.ToArray(), yValues.ToArray(), 100);
+
+                // Добавление точек на график
+                chart1.Series.Clear();
+                Series series = new Series
+                {
+                    ChartType = SeriesChartType.Point,
+                    MarkerStyle = MarkerStyle.Circle
+                };
+
+                for (int i = 0; i < xs.Length; i++)
+                {
+                    series.Points.AddXY(xs[i], ys[i]);
+                }
+
+                chart1.Series.Add(series);
+                chart1.Series.Add(seriesPoints);
+            }
+        }
+        private (double[], double[]) InterpolateSpline(double[] x, double[] y, int numPoints)
+        {
+            int n = x.Length;
+            double[] a = new double[n];
+            double[] b = new double[n - 1];
+            double[] d = new double[n - 1];
+            double[] h = new double[n - 1];
+
+            for (int i = 0; i < n; i++)
+                a[i] = y[i];
+
+            for (int i = 0; i < n - 1; i++)
+                h[i] = x[i + 1] - x[i];
+
+            double[] alpha = new double[n - 1];
+            for (int i = 1; i < n - 1; i++)
+                alpha[i] = (3 / h[i] * (a[i + 1] - a[i])) - (3 / h[i - 1] * (a[i] - a[i - 1]));
+
+            double[] c = new double[n];
+            double[] l = new double[n];
+            double[] mu = new double[n];
+            double[] z = new double[n];
+            l[0] = 1;
+            mu[0] = 0;
+            z[0] = 0;
+
+            for (int i = 1; i < n - 1; i++)
+            {
+                l[i] = 2 * (x[i + 1] - x[i - 1]) - h[i - 1] * mu[i - 1];
+                mu[i] = h[i] / l[i];
+                z[i] = (alpha[i] - h[i - 1] * z[i - 1]) / l[i];
+            }
+
+            l[n - 1] = 1;
+            z[n - 1] = 0;
+            c[n - 1] = 0;
+
+            for (int j = n - 2; j >= 0; j--)
+            {
+                c[j] = z[j] - mu[j] * c[j + 1];
+                b[j] = (a[j + 1] - a[j]) / h[j] - h[j] * (c[j + 1] + 2 * c[j]) / 3;
+                d[j] = (c[j + 1] - c[j]) / (3 * h[j]);
+            }
+
+            List<double> xs = new List<double>();
+            List<double> ys = new List<double>();
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                double step = (x[i + 1] - x[i]) / (double)numPoints;
+                for (double xi = x[i]; xi < x[i + 1]; xi += step)
+                {
+                    double yi = a[i] + b[i] * (xi - x[i]) + c[i] * Math.Pow((xi - x[i]), 2) + d[i] * Math.Pow((xi - x[i]), 3);
+                    xs.Add(xi);
+                    ys.Add(yi);
+                }
+            }
+
+            xs.Add(x[n - 1]);
+            ys.Add(a[n - 1]);
+
+            return (xs.ToArray(), ys.ToArray());
+        }
+    }
+}
